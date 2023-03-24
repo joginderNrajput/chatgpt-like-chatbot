b0c1b8cad846275e54a5a5b814f776f21ac0e2f9
Joginder Singh <85634213+joginderNrajput@users.noreply.github.com>
2023-03-25 03:06:34 +0530

    Update ResetBtn.js

--- src/components/ResetBtn.js
+++ src/components/ResetBtn.js
@@ -8,7 +8,7 @@ function ResetBtn(props){
       
 
     return(
-        <button onClick={handleReset} type="reset" className="inline-flex items-center px-4 py-2 bg-slate-200 border border-gray-300 rounded-md font-semibold text-sm text-gray-700 hover:text-gray-500 focus:outline-none focus:border-blue-500 active:bg-gray-50 active:text-gray-800 transition ease-in-out duration-150">
+        <button onClick={handleReset} type="reset" className="inline-flex items-center px-4 py-2 bg-slate-200 border border-blue-300 rounded-md font-semibold text-sm text-gray-700 hover:text-gray-200 hover:bg-blue-300 focus:outline-none transition ease-in-out duration-150">
         <img className="w-[1rem] mr-2" src="https://i.postimg.cc/tJTRMk2y/icons8-synchronize-48.png" alt="" />
 
   Reset
